b9f5a267267603ededa213b4a9055a4e6f57e97e
Clifford Yapp <238416+starseeker@users.noreply.github.com>
2024-04-23 16:45:06 -0400

    Not getting results from ADRT shotlining - not sure why yet.

--- perfcomp.c
+++ perfcomp.c
@@ -82,7 +82,7 @@ perfcomp(char *prefix, int argc, char **argv, int nthreads, int nproc,
     int i, j;
     clock_t cstart, cend;
     struct part **p;
-    struct retpack_s *r;
+    struct retpack_s *ret;
     struct timeval start, end;
     struct xray *ray;
     void *inst;
@@ -100,14 +100,14 @@ perfcomp(char *prefix, int argc, char **argv, int nthreads, int nproc,
     nthreads = nthreads;
     nproc = nproc;
 
-    r = (struct retpack_s *)malloc(sizeof(struct retpack_s));
-    if(r == NULL) return NULL;
+    ret = (struct retpack_s *)malloc(sizeof(struct retpack_s));
+    if(ret == NULL) return NULL;
 
     p = (struct part **)bu_malloc(sizeof(struct part *)*NUMTRAYS, "allocating partition space");
     ray = (struct xray *)bu_malloc(sizeof(struct xray)*(NUMTRAYS+1), "allocating ray space");
 
     inst = constructor(*argv, argc-1, argv+1);
-    if(inst == NULL) { free(r); return NULL; }
+    if(inst == NULL) { free(ret); return NULL; }
 
     /* first with a legit radius gets to define the bb and sph */
     /* XXX: should this lock? */
@@ -126,7 +126,7 @@ perfcomp(char *prefix, int argc, char **argv, int nthreads, int nproc,
 	VMOVE(ray->r_dir,dir[j]);
 	VJOIN1(ray->r_pt,bb[2],-radius,dir[j]);
 
-	r->p[j] = shoot(inst,ray);	/* shoot the accuracy ray while we're here */
+	ret->p[j] = shoot(inst,ray);	/* shoot the accuracy ray while we're here */
 
 	/* set up an othographic grid */
 	bn_vec_ortho( avec, ray->r_dir );
@@ -154,10 +154,10 @@ perfcomp(char *prefix, int argc, char **argv, int nthreads, int nproc,
 
     /* fill in the perfomrance data for the bundle */
 #define SEC(tv) ((double)tv.tv_sec + (double)(tv.tv_usec)/(double)1e6)
-    r->t = SEC(end) - SEC(start);
-    r->c = (double)(cend-cstart)/(double)CLOCKS_PER_SEC;
+    ret->t = SEC(end) - SEC(start);
+    ret->c = (double)(cend-cstart)/(double)CLOCKS_PER_SEC;
 
-    return r;
+    return ret;
 }
 
 /*
