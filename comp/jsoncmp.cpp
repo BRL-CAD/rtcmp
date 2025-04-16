8009c869048f29375be6f1d495c1d7fd46acd924
f4alt <christopherjmacgregor@gmail.com>
2025-04-16 18:10:05 -0400

    improve readability

--- comp/jsoncmp.cpp
+++ comp/jsoncmp.cpp
@@ -42,31 +42,20 @@ do_diff_run(const char *prefix, int argc, const char **argv, int nthreads, int r
 	int (*destructor) (void *),
 	CompareConfig& dinfo)
 {
-    struct part **p;
-    struct xray *ray;
     void *inst;
+    struct xray* rays;
+    int total_rays = NUMVIEWS * (rays_per_view + 1);	// rays per view + 1 accuracy ray per view
 
-    /* retained between runs. */
-    static double radius = -1.0;	/* bounding sphere and flag */
-    static point_t bb[3];	/* bounding box, third is center */
-
-    static vect_t dir[NUMVIEWS] = {
-	{0,0,1}, {0,1,0}, {1,0,0},	/* axis */
-	{1,1,1}, {1,4,-1}, {-1,-2,4}	/* non-axis */
-    };
-    for(int i=0;i<NUMVIEWS;i++) VUNITIZE(dir[i]); /* normalize the dirs */
-
-    nlohmann::json jshots;
-
-    ray = (struct xray *)bu_malloc(sizeof(struct xray)*((rays_per_view +1) * NUMVIEWS), "allocating ray space");
-
+    /* construct our application */
     inst = constructor(*argv, argc-1, argv+1, dinfo.json_ofile);
     if (inst == NULL) {
 	return;
     }
 
+    /* alloc expeceted rays */
+    rays = (struct xray *)bu_malloc(sizeof(struct xray) * total_rays, "allocating ray space");
+
     if (!dinfo.in_ray_file.empty()) {
-	// TODO: ray_file could have diff amount of rays than expected in the malloc a few lines up
 	// open file
 	std::ifstream rayfile(dinfo.in_ray_file, std::ios::binary);
 	if (!rayfile.is_open()) {
@@ -87,17 +76,19 @@ do_diff_run(const char *prefix, int argc, const char **argv, int nthreads, int r
 		std::istringstream iss(line);
 		std::string prefix;
 		double x, y, z;
+		// assumes line is point: xyz 123.4 234.5 345.6
+		//	    or direction: dir 123.4 234.5 345.6
 		iss >> prefix >> x >> y >> z;
 
 		if (prefix == "xyz") {
-		    ray[ray_idx].r_pt[0] = x;
-		    ray[ray_idx].r_pt[1] = y;
-		    ray[ray_idx].r_pt[2] = z;
+		    rays[ray_idx].r_pt[0] = x;
+		    rays[ray_idx].r_pt[1] = y;
+		    rays[ray_idx].r_pt[2] = z;
 		} else {
 		    // assume this is 'dir' line
-		    ray[ray_idx].r_dir[0] = x;
-		    ray[ray_idx].r_dir[1] = y;
-		    ray[ray_idx].r_dir[2] = z;
+		    rays[ray_idx].r_dir[0] = x;
+		    rays[ray_idx].r_dir[1] = y;
+		    rays[ray_idx].r_dir[2] = z;
 
 		    // got dir; next ray's up
 		    ray_idx++;
@@ -110,17 +101,30 @@ do_diff_run(const char *prefix, int argc, const char **argv, int nthreads, int r
 		size_t startIdx = line.find('[');
 		size_t endIdx = line.find(']');
 		if (startIdx != std::string::npos && endIdx != std::string::npos) {
-		    /* NOTE: hijack rays_per_view so our shoot() for-loop fires all the rays in here.
-			     we should end up with +NUMVIEWS rays as the 'accuracy rays' are shot and
-			     logged in the building of views
-		       TODO / FIXME: the bu_malloc of ray[] looks iffy
-		    */
-		    rays_per_view = stoi(line.substr(startIdx + 1, endIdx - startIdx - 1));
+		    // assume number in bracket is number of rays we need to read
+		    int num_rays = stoi(line.substr(startIdx + 1, endIdx - startIdx - 1));
+
+		    // ray file has more rays than we were expecting
+		    if (num_rays > total_rays) {
+			rays = (struct xray*)bu_realloc(rays, sizeof(struct xray) * num_rays, "realloc ray space");
+			total_rays = num_rays;
+		    }
 		}
 		in_section = true;
 	    }
 	}
     } else {
+	/* retained between runs. */
+	static double radius = -1.0;	/* bounding sphere and flag */
+	static point_t bb[3];		/* bounding box, third is center */
+
+	// TODO: better and/or random dirs?
+	static vect_t dir[NUMVIEWS] = {
+	    {0,0,1}, {0,1,0}, {1,0,0},			/* axis */
+	    {1,1,1}, {1,4,-1}, {-1,-2,4}		/* non-axis */
+	};
+	for(int i = 0; i < NUMVIEWS; ++i) VUNITIZE(dir[i]);	/* normalize the dirs */
+
 	/* first with a legit radius gets to define the bb and sph */
 	/* XXX: should this lock? */
 	if (radius < 0.0) {
@@ -131,42 +135,39 @@ do_diff_run(const char *prefix, int argc, const char **argv, int nthreads, int r
 	/* XXX: if locking, we can unlock here */
 
 	/* build the views with pre-defined rays, yo */
-	std::string accuracy_rays = "";
-	char buff[200];
+	int rays_per_ring = 100;
 	for(int j=0; j < NUMVIEWS; ++j) {
-	    vect_t avec,bvec;
+	    vect_t avec, bvec;
 
 	    // add accuracy ray in before bundle
 	    int acc_idx = j * (rays_per_view +1);
-	    VMOVE(ray[acc_idx].r_dir,dir[j]);
-	    VJOIN1(ray[acc_idx].r_pt, bb[2], -radius, dir[j]);
+	    VMOVE(rays[acc_idx].r_dir,dir[j]);
+	    VJOIN1(rays[acc_idx].r_pt, bb[2], -radius, dir[j]);
 
 	    /* set up an othographic grid */
-	    bn_vec_ortho( avec, ray->r_dir );
-	    VCROSS( bvec, ray->r_dir, avec );
+	    bn_vec_ortho( avec, rays[acc_idx].r_dir );
+	    VCROSS( bvec, rays[acc_idx].r_dir, avec );
 	    VUNITIZE( bvec );
-	    rt_raybundle_maker(ray + acc_idx,radius,avec,bvec,100,rays_per_view/100);
+	    rt_raybundle_maker(rays + acc_idx, radius, avec, bvec, rays_per_ring, rays_per_view/rays_per_ring);
 	}
 
 	// write all the ray info
 	// TODO: do this in dry-run?
-	if (true) {
-	    FILE* ray_file = fopen(dinfo.ray_file.c_str(), "w");	// intentionally erase contents if file already exists
-	    fprintf(ray_file, "**rays fired for %s [%d]**\n", dinfo.json_ofile.c_str(), rays_per_view + NUMVIEWS);	// bundle rays + accuracy rays (0-index)
-	    fprintf(ray_file, "%s", accuracy_rays.c_str());
-	    for (int i = 0; i < rays_per_view; i++) {
-		fprintf(ray_file, "xyz %0.17f %0.17f %0.17f\ndir %0.17f %0.17f %0.17f\n", V3ARGS(ray[i].r_pt), V3ARGS(ray[i].r_dir));
-	    }
-	    fclose(ray_file);
+	// TODO: write in chunks
+	FILE* ray_file = fopen(dinfo.ray_file.c_str(), "w");	// intentionally erase contents if file already exists
+	fprintf(ray_file, "**rays fired for %s [%d]**\n", dinfo.json_ofile.c_str(), total_rays);
+	for (int i = 0; i < total_rays; i++) {
+	    fprintf(ray_file, "xyz %0.17f %0.17f %0.17f\ndir %0.17f %0.17f %0.17f\n", V3ARGS(rays[i].r_pt), V3ARGS(rays[i].r_dir));
 	}
+	fclose(ray_file);
     }
 
     /* actually shoot all the pre-defined rays */
-    for(int i=0;i<(rays_per_view+1)*NUMVIEWS;++i)
-	shoot(inst,&ray[i]);
+    for(int i = 0; i < total_rays; ++i)
+	shoot(inst,&rays[i]);
 
     /* clean up */
-    bu_free(ray, "ray space");
+    bu_free(rays, "ray space");
     destructor(inst);
 }
 
